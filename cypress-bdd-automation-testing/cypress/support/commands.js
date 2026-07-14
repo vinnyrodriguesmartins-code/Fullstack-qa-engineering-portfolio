28355ae14a875c586a0e4732f3b2cd97e2ea3fe1
Vinny Rodrigues <vinnyrodriguesmartins@gmail.com>
2026-07-14 09:37:16 -0300

    clean: remove boilerplate Cypress comments and duplicates from support files

--- cypress-bdd-automation-testing/cypress/support/commands.js
+++ cypress-bdd-automation-testing/cypress/support/commands.js
@@ -1,41 +1,16 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-const variaveis = require('../fixtures/Configuracoes.js')
 import 'cypress-wait-until';
 import 'cypress-iframe';
 import 'cypress-drag-drop';
 import 'cypress-real-events/support';
 import 'cypress-xpath';
-import 'cypress-real-events/support';
 
-//Logar na Plataforma
+const variaveis = require('../fixtures/Configuracoes.js');
+
+// Comando customizado para realizar login via SSO na área do aluno
 Cypress.Commands.add('LoginNovaLLL_Marcos', () => {
   const emailareaaluno = variaveis.LoginNovaLLL_Marcos.email;
   const passwordareaaluno = variaveis.LoginNovaLLL_Marcos.password;
   cy.get('#email').type(emailareaaluno).wait(2000);
   cy.get('#password').type(passwordareaaluno).wait(2000);
-  cy.get('.submit-button').click({force: true}).wait(2000);
+  cy.get('.submit-button').click({ force: true }).wait(2000);
 });
